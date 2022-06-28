f20eda3ecec0c92245755beb6042790e755f7793
root <root@a10-52-40-5.qa.plesk.ru>
2022-06-29 01:22:53 +0700

    Added Vue component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!--link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"-->
 
         <style>
             body {
@@ -19,9 +19,12 @@
     </head>
 
     <body>
-
-    @yield('content')
+		<div id="app">
+			@yield('content')
+		</div>
+	<script src="{{ mix('js/app.js') }}"></script>
 
     </body>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"-->
+	
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,14 @@
                 <p>This awesome blog has many articles, click the button below to see them</p>
                 <br>
                 <a href="/blog" class="btn btn-outline-primary">Show Blog</a>
-            </div>
         </div>
     </div>
+</div>
+<div id="app">
+	<p>This part is made with Vue engine</p>
+	<template>
+		<count-click></count-click>	
+	</template>
+</div>
+
 @endsection
